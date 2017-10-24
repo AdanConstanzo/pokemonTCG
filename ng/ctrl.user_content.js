30d888880ff040f7d1c2a38b0e86d9070a3bc6c6
Adan Constanzo <aconstanzolan@gmail.com>
2017-10-23 23:40:23 -0700

    Working on user page. Going to bed.

--- ng/ctrl.user_content.js
+++ ng/ctrl.user_content.js
@@ -2,6 +2,7 @@
 // Can be accessed as /#/user-<userName>/
 app.controller("UserContentCtrl", function ($scope,$routeParams,UserSvc) {
     var user_content = {};
+    user_content.previouLi = "user_content_li_member";
     user_content.username = $routeParams.username;
     console.log(user_content.username);
     UserSvc.getUserOpenInfo(user_content.username).then(function(res){
@@ -21,4 +22,14 @@ app.controller("UserContentCtrl", function ($scope,$routeParams,UserSvc) {
             $scope.user_content_following = following.count;
         });
 
+    $scope.liClick = function (liId) {
+        "use strict";
+        if (liId !== user_content.previouLi) {
+            var previouLi = document.getElementById(user_content.previouLi);
+            previouLi.className = "";
+            var currentLid = document.getElementById(liId);
+            currentLid.className = "active";
+            user_content.previouLi = liId;
+        }
+    }
 });
