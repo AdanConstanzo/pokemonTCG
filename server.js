a15a8219de0b01e26b44042510a473c618a2fc2f
Adan Constanzo <aconstanzolan@gmail.com>
2017-11-27 01:37:41 -0800

    Fixed bug for registering accounts.

--- server.js
+++ server.js
@@ -2,22 +2,12 @@ var express    = require('express');
 var mongoose = require('mongoose');
 var app = express();
 app.use(require('./controllers'));
-app.set('port', (process.env.PORT || 3000));
+app.set('port', 3000);
 
-console.log(app.get('port'))
-var connection
+mongoose.connect("localhost:27017/gottatcg",function(){
+	console.log("mongoose is connected to localhost:27017/gottatcg")
+});
 
-if(app.get('port')=== 3000){
-	connection = 'localhost:27017/gottatcg'
-	mongoose.connect(connection,function(){
-		console.log("mongoose is connected")
-	})
-	app.listen(app.get('port'),function(){
-		console.log("app is running in localhost:3000")
-	})
-}
-else{
-	connection = "mongodb://danconable:supportftw117@ds147070.mlab.com:47070/gotta"
-	mongoose.connect(connection)
-	app.listen(app.get('port'))
-}
+app.listen(app.get('port'),function(){
+	console.log("app is running in localhost:3000")
+});
